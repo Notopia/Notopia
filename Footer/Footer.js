cec19cb30a55195a806ca4108aa0319f25bfd0ed
Notopia <xnotopiax@gmail.com>
2025-11-06 00:07:43 +0330

    Delete Footer directory

--- Footer/Footer.js
+++
@@ -1,7 +0,0 @@
-function registerInNews() {
-    let phoneNumber = document.getElementById("txtPhoneNumber").value;
-    let email = document.getElementById("txtEmail").value;
-    if (phoneNumber != "" && email != "") {
-        alert("درخواست شما با موفقیت ثبت شد.");
-    }
-}
