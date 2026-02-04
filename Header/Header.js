e36b58504463195e7ee838c322e364b4b775406f
Notopia <xnotopiax@gmail.com>
2026-02-04 19:17:32 +0330

    Add files via upload

---
+++ Header/Header.js
@@ -0,0 +1,23 @@
+function GoToDistancesPage() {
+    window.location.href = '../FormDistances/FormDistances.html'
+}
+function GoToHomePage() {
+    window.location.href = '../index.html'
+}
+function GoToContactPage() {
+    window.location.href = '../FormContactUs/FormContactUs.html'
+}
+let dropdownShowingStatus = false;
+function OpenDropdown() {
+    if (dropdownShowingStatus == false) {
+        document.getElementById("dropdown").classList.add("showDropdown");
+        dropdownShowingStatus = !dropdownShowingStatus;
+    }
+    else {
+        document.getElementById("dropdown").classList.remove("showDropdown");
+        dropdownShowingStatus = !dropdownShowingStatus;
+    }
+}
+function CloseDropdown() {
+    document.getElementById("dropdown").classList.remove("showDropdown");
+}
