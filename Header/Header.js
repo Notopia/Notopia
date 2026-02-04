d45517af9e75788701920adccf90682614c5e4cf
Notopia <xnotopiax@gmail.com>
2026-02-04 19:04:17 +0330

    Delete Header directory

--- Header/Header.js
+++
@@ -1,9 +0,0 @@
-function GoToDistancesPage() {
-    window.location.href = '../FormDistances/FormDistances.html'
-}
-function GoToHomePage() {
-    window.location.href = '../index.html'
-}
-function GoToContactPage() {
-    window.location.href = '../FormContactUs/FormContactUs.html'
-}
