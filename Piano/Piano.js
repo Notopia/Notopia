08b5e617bac0b0ab58041a7e0099c8c41f028491
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-08-12 00:44:27 +0330

    Add vitural piano

--- Piano/Piano.js
+++ Piano/Piano.js
@@ -1,3 +1,23 @@
+function MainButtonMousedownHandler(event) {
+    const clickedButton = event.currentTarget;
+    clickedButton.classList.remove("pianoMainButtonShadow");
+    const audio = new Audio(`/Audio/${event.currentTarget.id}`)
+    audio.play();
+}
+function MainButtonMouseupHandler(event) {
+    const clickedButton = event.currentTarget;
+    clickedButton.classList.add("pianoMainButtonShadow");
+}
+function PrimaryButtonMousedownHandler(event) {
+    const clickedButton = event.currentTarget;
+    clickedButton.classList.remove("pianoPrimaryButtonShadow");
+    const audio = new Audio(`/Audio/${event.currentTarget.id}`)
+    audio.play();
+}
+function PrimaryButtonMouseupHandler(event) {
+    const clickedButton = event.currentTarget;
+    clickedButton.classList.add("pianoPrimaryButtonShadow");
+}
 // -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const selectedSectionHeader = document.querySelector(".header__sections__piano");
 const selectedSectionDropdown = document.querySelector("#pianoPart");
