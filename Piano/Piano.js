6fcb4400b223c511eabe343f5643857c65bc2fe6
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-08-12 13:34:42 +0330

    Add audios to piano

--- Piano/Piano.js
+++ Piano/Piano.js
@@ -1,24 +1,33 @@
+//*Preload audios
+const audios = {};
+for (let i = 1; i <= 12; i++) {
+    audios[i] = new Audio(`/Audios/${i}.mp3`);
+}
+//*Handle click on mains
 function MainButtonMousedownHandler(event) {
     const clickedButton = event.currentTarget;
     clickedButton.classList.remove("pianoMainButtonShadow");
-    const audio = new Audio(`/Audio/${event.currentTarget.id}`)
-    audio.play();
+    PlayAudio(clickedButton.id)
 }
 function MainButtonMouseupHandler(event) {
     const clickedButton = event.currentTarget;
     clickedButton.classList.add("pianoMainButtonShadow");
 }
+//*Handle click on primaries
 function PrimaryButtonMousedownHandler(event) {
     const clickedButton = event.currentTarget;
     clickedButton.classList.remove("pianoPrimaryButtonShadow");
-    const audio = new Audio(`/Audio/${event.currentTarget.id}`)
-    audio.play();
+    PlayAudio(clickedButton.id)
 }
 function PrimaryButtonMouseupHandler(event) {
     const clickedButton = event.currentTarget;
     clickedButton.classList.add("pianoPrimaryButtonShadow");
 }
-// -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//*Play audio
+function PlayAudio(id) {
+    audios[id].play()
+}
+//!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const selectedSectionHeader = document.querySelector(".header__sections__piano");
 const selectedSectionDropdown = document.querySelector("#pianoPart");
 (function () {
