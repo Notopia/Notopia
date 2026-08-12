2f86f26ae80c3e6ccafb5b087875cd6109f9c50a
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-08-12 21:41:06 +0330

    Add piano box in home page

--- Home/Home.js
+++ Home/Home.js
@@ -1,23 +1,36 @@
 const distancesPartTitle = document.getElementById("distancesPartTitle")
-const SVGArrow = document.getElementById("ui-gambling-website-lined-icnos-casinoshunter")
-function MouseOverHandler() {
+const SVGArrowDistances = document.getElementById("ui-gambling-website-lined-icnos-casinoshunter")
+function MouseOverDistancesHandler() {
     distancesPartTitle.style.color = "#f9a515"
-    SVGArrow.style.fill = "#f9a515";
+    SVGArrowDistances.style.fill = "#f9a515";
 }
-function MouseLeaveHandler() {
+function MouseLeaveDistancesHandler() {
     distancesPartTitle.style.color = "rgba(255, 255, 255, 1)"
-    SVGArrow.style.fill = "rgba(255, 255, 255, 1)";
+    SVGArrowDistances.style.fill = "rgba(255, 255, 255, 1)";
 }
+//!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 const stepsPartTitle = document.getElementById("stepsPartTitle")
-const SVGArrow2 = document.getElementById("ui-gambling-website-lined-icnos-casinoshunter2")
-function MouseOverHandler2() {
+const SVGArrowSteps = document.getElementById("ui-gambling-website-lined-icnos-casinoshunter2")
+function MouseOverStepsHandler() {  
     stepsPartTitle.style.color = "#f9a515"
-    SVGArrow2.style.fill = "#f9a515";
+    SVGArrowSteps.style.fill = "#f9a515";
 }
-function MouseLeaveHandler2() {
+function MouseLeaveStepsHandler() {
     stepsPartTitle.style.color = "rgba(255, 255, 255, 1)"
-    SVGArrow2.style.fill = "rgba(255, 255, 255, 1)";
+    SVGArrowSteps.style.fill = "rgba(255, 255, 255, 1)";
 }
+//!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const pianoPartTitle = document.getElementById("pianoPartTitle")
+const SVGArrowPiano = document.getElementById("ui-gambling-website-lined-icnos-casinoshunter3")
+function MouseOverPianoHandler() {
+    pianoPartTitle.style.color = "#f9a515"
+    SVGArrowPiano.style.fill = "#f9a515";
+}
+function MouseLeavePianoHandler() {
+    pianoPartTitle.style.color = "rgba(255, 255, 255, 1)"
+    SVGArrowPiano.style.fill = "rgba(255, 255, 255, 1)";
+}
+//!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function ChangePage() {
     loading.classList.remove("Lhidden");
     loading.classList.add("Lflex");
