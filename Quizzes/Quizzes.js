da677817866d56b02a1233b1ed2e0b6467cb2342
Alireza Sadeghi <alirezagt4x@gmail.com>
2026-08-17 00:27:51 +0330

    Add Quizzes page's basics

---
+++ Quizzes/Quizzes.js
@@ -0,0 +1,7 @@
+//!-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+const selectedSectionHeader = document.querySelector(".header__sections__quizzes");
+const selectedSectionDropdown = document.querySelector("#quizzesPart");
+(function () {
+    selectedSectionHeader.classList.add("selectedDiv");
+    selectedSectionDropdown.classList.add("selectedDiv");
+})();
